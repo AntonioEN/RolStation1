ada3e8fead7a4d4885653a2529a2d29c7d789c91
trackson12 <angelpeke1999@hotmail.com>
2020-01-29 21:48:16 +0100

    intento de acceder al ND con el marker

--- app/src/main/java/com/example/rolstation/MapsActivity.java
+++ app/src/main/java/com/example/rolstation/MapsActivity.java
@@ -1,28 +1,58 @@
 package com.example.rolstation;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+    private View root;
+    private DrawerLayout drawerLayout;
     private GoogleMap mMap;
+    private Marker marcadorTouch;
+    private static final int LOCATION_REQUEST_CODE = 1;
+    public MapsActivity() {
+    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(LayoutInflater inflater, ViewGroup container,
+                            Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        root = inflater.inflate(R.layout.activity_maps, container, false);
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+            }
+
+        });
+
     }
 
 
@@ -35,13 +65,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("WrongConstant")
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
+        mMap = googleMap;
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(38.692972, -4.108641);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("qUE NO ES SIDNEY"));
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Que no es Sidney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
